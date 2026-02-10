ed610c83133d9d7003a81005227526588560d66b
unknown <bdcpas19@gmail.com>
2026-02-10 14:18:47 +0800

    Done with Profile Demo

---
+++ react-native-getting-started/ProfileDemo.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const ProfileDemo = () => {
+  return (
+    <View>
+      <Text>Course & Section : BSIS 3-B</Text>
+      <Text>Name : Patron, Mark Jason L.</Text>
+      <Text>Age : 21</Text>
+      <Text>Favorite Hobby : Reading Manga</Text>
+      <Text>My Three Pet peeves in class/classmate: Being ignored, Getting pointed during recitation, Standing out</Text>
+    </View>
+  );
+};
+
+export default ProfileDemo;
